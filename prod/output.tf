a6b3611bb9bd70010031ee1cb0c2bf0cba426a3d
lsh23 <shseoul14@gmail.com>
2022-10-16 19:02:16 +0900

    Upload module usage example - prod

---
+++ prod/output.tf
@@ -0,0 +1,7 @@
+output "instance_private_ip_list" {
+    value = module.instance.instance_private_ip_info
+}
+
+output "instance_uuid_info" {
+    value = module.instance.instance_uuid_info
+}
---
+++ prod/terraform.tfvars
@@ -0,0 +1,9 @@
+names = ["instance-1", "instance-2"]
+
+ami = "ami-0c76973fbe0ee100c"
+
+instance_type = "t2.micro"
+
+cidr_block = "10.0.0.0/16"
+
+key_name = "prod-key"
